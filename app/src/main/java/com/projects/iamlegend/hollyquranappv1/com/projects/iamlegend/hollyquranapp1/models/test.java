fc35f282cb530b3d696688f8cd5b33575df682de
ahmedmoheb5594 <ahmedmoheb500@gmail.com>
2019-06-18 12:17:00 +0200

    New Attribute added and Aditional parametrized constructor added

--- app/src/main/java/com/projects/iamlegend/hollyquranappv1/com/projects/iamlegend/hollyquranapp1/models/test.java
+++ app/src/main/java/com/projects/iamlegend/hollyquranappv1/com/projects/iamlegend/hollyquranapp1/models/test.java
@@ -7,10 +7,16 @@ package com.projects.iamlegend.hollyquranappv1.com.projects.iamlegend.hollyquran
 public class test {
 
     String myTestAttribute;
+    String myTestAttribute2;
     public test() {
     }
 
     public test(String myTestAttribute) {
         this.myTestAttribute = myTestAttribute;
+    }
+
+    public test(String myTestAttribute, String myTestAttribute2) {
+        this.myTestAttribute = myTestAttribute;
+        this.myTestAttribute2 = myTestAttribute2;
     }
 }
